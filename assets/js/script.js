3171a3dd3d48bfa377c9408967f50cf352925a4f
Is2dann <144619269+Is2dann@users.noreply.github.com>
2024-04-23 15:19:28 +0100

    add js code for DOM content load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+// This is just for a bit of fun
+console.log('I am here first, but...');
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("now we have the DOM fully loaded and parsed");
+  });
+
