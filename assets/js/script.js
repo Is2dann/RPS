a1c84765f585635e8989f64b46d3891b04ee041a
Is2dann <144619269+Is2dann@users.noreply.github.com>
2024-04-23 19:52:16 +0100

    add losing and draw scenario function, correct indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,26 +21,42 @@ let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 
- // give the computer the opportunity to choose
+  // give the computer the opportunity to choose
 function npcChoice() {
-    let choices = ['rock','paper','scissor'];
-    // to choose between 0 and 3 randomly
-    let randomize = Math.floor(Math.random() * 3);
-    // but to choose only the options we gave (rock, paper, scissors)
-    return choices[randomize];
+  let choices = ['rock','paper','scissor'];
+  // to choose between 0 and 3 randomly
+  let randomize = Math.floor(Math.random() * 3);
+  // but to choose only the options we gave (rock, paper, scissors)
+  return choices[randomize];
 }
 
   // set the scenarios
   // if Win
 function winner(yourChoice, opponentChoice) {
-    // increment the player's score by 1
-    yourScore++;
-    // and display it in the score board area
-    yourScoreTag.innerHTML = yourScore;
-    // opponent score does not change, but display it anyway
-    opponentScoreTag.innerHTML = opponentScore;
-    // change the message accordingly to the game result with template literals
-    resultMsg.innerHTML = `${yourChoice} tops ${opponentChoice}. You are a Winner!`;
+  // increment the player's score by 1
+  yourScore++;
+  // and display it in the score board area
+  yourScoreTag.innerHTML = yourScore;
+  // opponent score does not change, but display it anyway
+  opponentScoreTag.innerHTML = opponentScore;
+  // change the message accordingly to the game result with template literals
+  resultMsg.innerHTML = `${yourChoice} tops ${opponentChoice}. You are a Winner!`;
+}
+
+  // if Lose
+function loser(yourChoice, opponentChoice) {
+  // increment the opponent's score by 1
+  opponentScore++;
+  yourScoreTag.innerHTML = yourScore;
+  opponentScoreTag.innerHTML = opponentScore;
+  resultMsg.innerHTML = `${yourChoice} loses ${opponentChoice}. You are a loser!`;
+}
+
+  // if it's a draw
+function drawer(yourChoice, opponentChoice) {
+  // no scores going up, so scoreboard does not need to update
+  // message still need to be updated accordingly
+  resultMsg.innerHTML = `${yourChoice} equals ${opponentChoice}. It's a draw!`;
 }
 
   // let's make the game play
